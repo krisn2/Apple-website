dcf2b6da008d4256d53b9f0afd300854866ed7c0
krisn2 <krishnasarone08@gmail.com>
2025-01-07 23:00:40 +0530

    init the full project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./constants/Navbar"
+import Hero from "./constants/Hero"
+import Highlights from "./constants/Highlights"
+
+const App =  () => {
+  return (
+   <main className="bg-black">
+    <Navbar />
+    <Hero />
+    <Highlights />
+   </main>
+  )
+}
+
+export default App
