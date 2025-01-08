2859574532bc1261d4436816b8ee2533c9fe34b8
krisn2 <krishnasarone08@gmail.com>
2025-01-08 16:04:03 +0530

    videocarousel done and fix folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Navbar from "./constants/Navbar"
-import Hero from "./constants/Hero"
-import Highlights from "./constants/Highlights"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
 
 const App =  () => {
   return (
